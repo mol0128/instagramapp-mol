4f0845b5d1258c7fddc9eaafaa3917191af7bfcf
室井博也 <>
2020-10-05 17:12:28 +0900

    active_storage、プロフィールページ

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,25 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @profile = current_user.prepare_profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新！'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :show
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :name,
+      :avatar
+    )
   end
 end
